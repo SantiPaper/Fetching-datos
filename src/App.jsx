650b2defc154ba4d70360fc291764e1527e812c8
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-07-15 19:20:40 -0300

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
+import { AppRouter } from "./AppRouter";
+import { PokemonProvider } from "./context/PokemonProvider";
 
 function App() {
-  
-
   return (
     <>
-
+      <PokemonProvider>
+        <AppRouter/>
+      </PokemonProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from "react-redux"
-import store from "./redux/store"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "./index.css"
+import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>,
-
-  document.getElementById('root')
-);
+)
